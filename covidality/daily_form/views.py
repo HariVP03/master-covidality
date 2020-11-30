2d38b08968d8b5c849e22f6b74b7ae91383fa340
Hari Vishnu Parashar <harryskotch11@gmail.com>
2020-11-30 13:21:15 +0530

    User can now directly add data to the database from the website

--- covidality/daily_form/views.py
+++ covidality/daily_form/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 
+import mysql.connector
+
+database = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='password',
+    database='covidality'
+)
+
+cursor = database.cursor()
+
+def inputData(name, age, initialTemp, finalTemp, emp_id='NULL', feeling=5):
+
+    # Getting the timestamp (date, time) at the time of data entry
+    # and entering it in the time_stamp column
+    cursor.execute('SELECT current_timestamp()')
+    current_timestamp_obj = cursor.fetchone()
+    current_timestamp = str(current_timestamp_obj[0])
+
+    cursor.execute(f'INSERT INTO daily_data VALUES {name, age, initialTemp, finalTemp, emp_id, feeling, current_timestamp}')
+    database.commit()
 
 def fill_daily_form(request):
     return render(request, 'daily form.html')
@@ -13,5 +34,7 @@ def fill_form(request):
     temp2 = request.POST['temp2']
     emp_id = request.POST['emp_id']
     feeling = request.POST['feeling']
+
+    inputData(name, age, temp1, temp2, emp_id, feeling)
 
     return render(request, 'submission-received.html')
